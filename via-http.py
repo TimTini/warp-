b5a70efa4207df538245b37a4e6cdd8b2176d477
TimTini <48094158+TimTini@users.noreply.github.com>
2021-03-07 23:34:13 +0700

    Add files via upload

--- via-http.py
+++ via-http.py
@@ -1,4 +1,4 @@
 from common import WrapPlus
 _REFERRER = "5fb12dd5-2cd6-474a-ae58-d6bd5ec2f51b"
-boots_with_http = WrapPlus(referrer=_REFERRER,protocol='http',timeout=1000)
+boots_with_http = WrapPlus(referrer=_REFERRER,protocol='http',timeout=2000)
 boots_with_http.run()
