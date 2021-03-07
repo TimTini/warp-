f2f8fde0d0b29db77730b5a07dc94ae3b5d0c71e
TimTini <48094158+TimTini@users.noreply.github.com>
2021-03-07 19:31:58 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+files = [
+    'via-http.py',
+    'via-socks4.py',
+    'via-socks5.py'
+]
+for filei in files:
+    os.startfile(f'{filei}')
--- via-socks4.py
+++ via-socks4.py
@@ -1,4 +1,4 @@
 from common import WrapPlus
 _REFERRER = "5fb12dd5-2cd6-474a-ae58-d6bd5ec2f51b"
-boots_with_http = WrapPlus(referrer=_REFERRER,protocol='socks4',timeout=100)
+boots_with_http = WrapPlus(referrer=_REFERRER,protocol='socks4',timeout=500)
 boots_with_http.run()
